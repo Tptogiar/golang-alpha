04b426abe551cbad185245c45a61cf689ac44995
Tptogiar <2528891112@qq.com>
2022-11-27 14:43:45 +0800

    add redis_rate test

---
+++ three-party-libraries/go-redis.redis_rate/redis_rate_test.go
@@ -0,0 +1,31 @@
+package go_redis_redis_rate
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/go-redis/redis_rate/v9"
+	"testing"
+	"time"
+)
+
+func TestAlphaTest(t *testing.T) {
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "175.178.7.180:9004",
+	})
+
+	limiter := redis_rate.NewLimiter(rdb)
+	res, err := limiter.Allow(ctx, "project:123", redis_rate.PerSecond(10))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("allowed", res.Allowed, "remaining", res.Remaining)
+	fmt.Println("allowed", res.Allowed, "remaining", res.Remaining)
+	time.Sleep(1)
+	fmt.Println("allowed", res.Allowed, "remaining", res.Remaining)
+	time.Sleep(1)
+	fmt.Println("allowed", res.Allowed, "remaining", res.Remaining)
+}
